7c250c98a15d273411dcb697cb1917335050a924
Taif Rahaman <taif.rahaman73@gmail.com>
2020-12-06 20:40:01 +0600

    added homepage and productpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
-
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 const App = () => {
-  return <div>Hi</div>;
+  return (
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/product/:id" component={ProductPage} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
---
+++ client/src/pages/ProductPage.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Col, Container, Image, ListGroup, Row } from 'react-bootstrap';
+
+import Rating from '../components/Rating';
+import products from '../data/products';
+
+const ProductPage = ({ match }) => {
+  const product = products.find((p) => p._id === match.params.id);
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={4}>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h3>{product.name}</h3>
+              </ListGroup.Item>
+            </ListGroup>
+            <ListGroup.Item style={{ border: 'none' }}>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item style={{ border: 'none' }}>
+              <h5> Price: ${product.price}</h5>
+            </ListGroup.Item>
+            <ListGroup.Item style={{ border: 'none' }}>
+              {product.description}
+            </ListGroup.Item>
+            <ListGroup.Item
+              className={
+                product.countInStock > 0 ? 'text-success' : 'text-danger'
+              }
+              style={{ border: 'none' }}
+            >
+              <strong>
+                {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+              </strong>
+            </ListGroup.Item>
+            <ListGroup.Item style={{ border: 'none' }}>
+              <Button
+                className="btn-block"
+                variant="outline-info"
+                disabled={product.countInStock === 0}
+                style={{ borderRadius: '50px' }}
+              >
+                Add to Cart
+              </Button>
+            </ListGroup.Item>
+          </Col>
+        </Row>
+        <Link
+          className="btn btn-outline-dark my-5 "
+          style={{ borderRadius: '50px' }}
+          to="/"
+        >
+          Go Back
+        </Link>
+      </Container>
+    </>
+  );
+};
+
+export default ProductPage;
