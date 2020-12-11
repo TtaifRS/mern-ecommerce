00c8d17a4d63f8aba504b7afa9f06644f4f61535
Taif Rahaman <taif.rahaman73@gmail.com>
2020-12-11 13:20:50 +0600

    user auth with login, register and private profile access

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import colors from 'colors';
 
 import connectDB from './config/database.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middlewares/errorMiddleware.js';
 
 dotenv.config();
@@ -12,7 +13,11 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.use('/api/products', productRoutes);
+
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 
