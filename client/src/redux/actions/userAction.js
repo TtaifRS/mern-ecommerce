e3cea718ae57fb2850a4851d44c03161eea7c69e
Taif Rahaman <taif.rahaman73@gmail.com>
2020-12-11 19:34:59 +0600

    register page added

--- client/src/redux/actions/userAction.js
+++ client/src/redux/actions/userAction.js
@@ -3,6 +3,9 @@ import {
   USER_LOGIN_REQ,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQ,
+  USER_REGISTER_SUCCESS,
 } from '../types/userTypes';
 import axios from 'axios';
 
@@ -46,4 +49,42 @@ export const logout = () => async (dispatch) => {
     type: USER_LOGOUT,
   });
   localStorage.removeItem('userInfo');
+};
+
+export const register = (name, email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_REGISTER_REQ,
+    });
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const { data } = await axios.post(
+      '/api/users',
+      { name, email, password },
+      config
+    );
+
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data,
+    });
+
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
+
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -6,13 +6,14 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers';
 import { cartRecucer } from './reducers/cartReducers';
-import { userLoginReducer } from './reducers/userReducers';
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
   productList: productListReducers,
   productDetails: productDetailsReducer,
   cart: cartRecucer,
   userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
